ec0850229f3b9b1c904517aaa45d8d5719527fb0
scivision <scivision@users.noreply.github.com>
2014-09-16 12:06:50 -0400

    terse output

--- analyzeforms.py
+++ analyzeforms.py
@@ -7,25 +7,28 @@ http://stackoverflow.com/questions/22093006/python-pandas-filter-dataframe-by-ap
 http://pandas.pydata.org/pandas-docs/version/0.8.1/missing_data.html
 '''
 
-def analyzeForms(xlsfn,mplots,pick):
+def analyzeForms(xlsfn,mplots,pick,full):
     pd.options.display.max_colwidth=17
 
     xlsfn = expanduser(xlsfn)
     data = pd.read_excel(xlsfn)
 
     #this can be looped in a smarter way, but just for clarity...
-    rechoice(data,pick[0],'choice one')
-    rechoice(data,pick[1],'choice two')
-    rechoice(data,pick[2],'choice three')
+    rechoice(data,pick[0],'choice one',full)
+    rechoice(data,pick[1],'choice two',full)
+    rechoice(data,pick[2],'choice three',full)
 
     return data
 
-def rechoice(data,choice,choicenum):
+def rechoice(data,choice,choicenum,full):
     if choice is not None:
         dsl = data[ data[choicenum].str.contains(choice).fillna(False) ]
         nsl = dsl.shape[0]
         print(str(nsl) + ' students chose ' + choice + ' as ' + choicenum)
-        print(dsl.ix[:,1:].to_string(justify='left'))
+        if full:
+            print(dsl.ix[:,1:].to_string(justify='left'))
+        else:
+            print(dsl.ix[:,1:3].to_string(justify='left'))
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -36,6 +39,7 @@ if __name__ == '__main__':
     p.add_argument('--p1',help='list who picked for project one this project choice',type=str,default=None)
     p.add_argument('--p2',help='list who picked for project two this project choice',type=str,default=None)
     p.add_argument('--p3',help='list who picked for project three this project choice',type=str,default=None)
+    p.add_argument('--full',help='print maximum amount of detail about respondant',action='store_true')
 
     ar = p.parse_args()
     pick = [ar.p1, ar.p2, ar.p3]
@@ -45,8 +49,8 @@ if __name__ == '__main__':
         from readprofiler import goCprofile
         profFN = 'analyzeforms.pstats'
         print('saving profile results to ' + profFN)
-        cProfile.run('analyzeForms(ar.infile,ar.plot, pick)',profFN)
+        cProfile.run('analyzeForms(ar.infile,ar.plot, pick,ar.full)',profFN)
         goCprofile(profFN)
     else:
-        data = analyzeForms(ar.infile, ar.plot, pick)
+        data = analyzeForms(ar.infile, ar.plot, pick,ar.full)
         #print(data)
