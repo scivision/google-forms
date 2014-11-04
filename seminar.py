3a75fc0a4c9d7c94fc37ed3ed2f9743d34138916
scivision <scivision@users.noreply.github.com>
2014-11-03 21:58:10 -0500

    allow printing name

--- seminar.py
+++ seminar.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python3
 import pandas as pd
 from os.path import expanduser
+from numpy import s_
+from pdb import set_trace
 '''Michael Hirsch 2014
 '''
 
-def analyzeForms(xlsfn):
+def analyzeForms(xlsfn,name):
     pd.options.display.max_colwidth=17
 
     data = pd.read_excel(expanduser(xlsfn))
 
-    semrep(data)
+    semrep(data,name)
 
     return data
 
-def semrep(data):
+def semrep(data,name):
+
     numeach = data.ix[:,3:].sum(axis=1,numeric_only=True)
+    if name:
+        di = s_[:2]
+       # set_trace()
+        print("NO seminars:",numeach.isnull().sum() )
+        print(data.ix[numeach.isnull(),di].values,sep=';')
 
+        print('one seminar:',(numeach==1).sum())
+        print(data.ix[numeach==1,di].values,sep=';')
 
+        print('two seminars:',(numeach==2).sum())
+        print(data.ix[numeach==2,di].values,sep=';')
+    else: #email
+        di = 2
+        print("NO seminars:",numeach.isnull().sum() )
+        print(*data.ix[numeach.isnull(),di].tolist(),sep=';')
 
-    print("students who have attended NO seminars yet: " + str(numeach.isnull().sum()))
-    print(*data.ix[numeach.isnull(),2].tolist(),sep=';')
-    print('------------------------------')
+        print('one seminar:',(numeach==1).sum())
+        print(*data.ix[numeach==1,di].tolist(),sep=';')
 
-
-    print('students that attended exactly one seminar: ' + str((numeach==1).sum()))
-    print(*data.ix[numeach==1,2].tolist(),sep=';')
-    print('------------------------------')
-
-    print('students that attended exactly two seminars: ' + str((numeach==2).sum()))
-    print(*data.ix[numeach==2,2].tolist(),sep=';')
-    print('------------------------------')
+        print('two seminars:',(numeach==2).sum())
+        print(*data.ix[numeach==2,di].tolist(),sep=';')
 
 
 
@@ -37,6 +46,7 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='Analyzes attendance statistics')
     p.add_argument('infile',help='.xls filename',type=str)
+    p.add_argument('--name',help='print name instead of email',action='store_true')
     ar = p.parse_args()
 
-    data = analyzeForms(ar.infile)
+    data = analyzeForms(ar.infile,ar.name)
